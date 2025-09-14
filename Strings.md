8071f065074627438faab53ce7c51f92bcf31d37
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-14 18:26:58 +0900

    ˆ

--- Strings.md
+++ Strings.md
@@ -4,4 +4,30 @@
 
 --- 
 
+### INIT
 
+- gradle 초기 import문
+![StringsInit.png](images/StringsInit.png)
+
+- main에서의 사용 (try문은 .cloes() 하려고   스코프를 만든것 마지막에 .close() 하면댐
+![Strings_main.png](images/Strings_main.png)
+
+
+- 결과
+
+![Strings_main_redult.png](images/Strings_main_redult.png)
+![StringsInterminal.png](images/StringsInterminal.png)
+![Strings_monitor.png](images/Strings_monitor.png)
+
+
+
+---
+
+### MGET 
+![MGet.png](images/MGet.png)
+![MGet1.png](images/MGet1.png)
+
+
+- 결과 
+![MgetInSout.png](images/MgetInSout.png)
+![Mget_1.png](images/Mget_1.png)
--- StringsP/src/main/java/com/teambind/Main.java
+++ StringsP/src/main/java/com/teambind/Main.java
@@ -1,17 +1,33 @@
 package com.teambind;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.List;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+        try{
+            JedisPool jedisPool = new JedisPool("127.0.0.1", 6379);
+
+            try{
+                Jedis jedis = jedisPool.getResource();
+                jedis.set("users:100:email", "ddingsha9@teambind.co.kr");
+                jedis.set("users:100:name", "dding joo ");
+                jedis.set("users:200:age", "20");
+                String value = jedis.get("users:100:email");
+                System.out.println(value);
+
+                List<String> val  = jedis.mget("users:100:email", "users:100:name", "users:200:age");
+                System.out.println(val.toString());
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
