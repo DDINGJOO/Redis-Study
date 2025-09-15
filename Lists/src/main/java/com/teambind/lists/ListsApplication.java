28d0a50cd3084744498f55fe84dedd9d61662aaf
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-15 17:41:33 +0900

    Add pipelining example and update documentation
    - Extend `Main.java` with Redis pipelining examples for improved performance demonstration.
    - Update `ReadMe.md` and `Strings.md` with additional Redis pipelining details.
    - Add new images to illustrate pipelining usage.

--- Lists/src/main/java/com/teambind/lists/ListsApplication.java
+++ Lists/src/main/java/com/teambind/lists/ListsApplication.java
@@ -1,13 +1,11 @@
 package com.teambind.lists;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class ListsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ListsApplication.class, args);
+        System.out.println("Hello World!");
+
+
     }
 
 }
