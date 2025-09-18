99d79e93bd031d4f185461a4069cce5d7913280d
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-18 17:35:09 +0900

    Initialize new project `jedischache`
    - Add Gradle wrapper, build configuration, and dependencies.
    - Include core Spring Boot application setup and sample entities.
    - Configure `.gitignore`, `.gitattributes`, and `application.yaml`.
    - Update `UserController` with Redis TTL functionality.
    - Add test classes and update documentation with sample images.

--- cache/src/main/java/com/fs/cache/CacheApplication.java
+++ cache/src/main/java/com/fs/cache/CacheApplication.java
@@ -1,13 +1,41 @@
 package com.fs.cache;
 
+import com.fs.cache.domain.entity.User;
+import com.fs.cache.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class CacheApplication {
-
+@RequiredArgsConstructor
+@EnableJpaAuditing
+public class CacheApplication implements ApplicationRunner {
+    private final UserRepository userRepository;
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        userRepository.save(User.builder()
+                        .name("grage")
+                        .email("grage@example.com")
+                        .build());
+        userRepository.save(User.builder()
+                .name("bob")
+                .email("bob@example.com")
+                .build());
+        userRepository.save(User.builder()
+                .name("Alice")
+                .email("Alice@example.com")
+                .build());
+        userRepository.save(User.builder()
+                .name("Rylen")
+                .email("Rylen@example.com")
+                .build());
+
+    }
 }
---
+++ cache/src/main/java/com/fs/cache/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fs.cache.domain.repository;
+
+
+import com.fs.cache.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long>{
+}
